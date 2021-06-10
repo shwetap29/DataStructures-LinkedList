d3cf13cf67795dcc0d484f77ef4aec0dfbef68ef
shwetap29 <shwetaprasad48@gmail.com>
2021-06-10 19:20:26 +0530

    REFACTORED:Append to 30 & 70 To 56

--- src/main/java/com/bridgelabz/MyLinkedList.java
+++ src/main/java/com/bridgelabz/MyLinkedList.java
@@ -3,12 +3,13 @@ package com.bridgelabz;
 public class MyLinkedList {
     public static void main(String[] args){
         LinkedList linkedList1= new LinkedList();
-        linkedList1.add(70);
-        linkedList1.add(30);
-        linkedList1.add(56);
+        linkedList1.append(70);
+        linkedList1.append(30);
+        linkedList1.append(56);
         linkedList1.printList();
     }
 }
+
  class LinkedList{
     Node head;
 
@@ -22,13 +23,31 @@ public class MyLinkedList {
                 next = null;
             }
         }
+        /*
         //Method To Add elements To linked List
         public void add (int data){
             Node newNode = new Node(data);
             newNode.next=head;
             head=newNode;
         }
-        // Method To print Linked List
+        */
+        // Method Of Append
+     public void append(int data) {
+         Node newNode = new Node(data);
+         if(head == null ){
+             head = newNode;
+             return;
+         }
+         Node last = head;
+         while (last.next !=null) {
+             last = last.next;
+         }
+         last.next=newNode;
+     }
+
+
+
+     // Method To print Linked List
         public void printList(){
             System.out.println("LinkedList:");
             Node temp=head;
