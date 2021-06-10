362eee1766a6ca950060eda9a6a5e7b9fd5d120a
shwetap29 <shwetaprasad48@gmail.com>
2021-06-10 20:13:10 +0530

    REFACTORED:To get 30 in Between 56 & 70

--- src/main/java/com/bridgelabz/MyLinkedList.java
+++ src/main/java/com/bridgelabz/MyLinkedList.java
@@ -3,9 +3,9 @@ package com.bridgelabz;
 public class MyLinkedList {
     public static void main(String[] args){
         LinkedList linkedList1= new LinkedList();
+        linkedList1.append(56);
         linkedList1.append(70);
         linkedList1.append(30);
-        linkedList1.append(56);
         linkedList1.printList();
     }
 }
@@ -31,6 +31,7 @@ public class MyLinkedList {
             head=newNode;
         }
         */
+
         // Method Of Append
      public void append(int data) {
          Node newNode = new Node(data);
@@ -45,7 +46,17 @@ public class MyLinkedList {
          last.next=newNode;
      }
 
+     // Method To Insert Element 30 between 56 and 70
+        public void insertAfter(Node prevNode, int data) {
+            if (prevNode == null) {
+                    System.out.println("previous node should not be null");
+             return;
+         }
+         Node newNode = new Node(data);
+         newNode.next = prevNode.next;
+         prevNode.next = newNode;
 
+        }
 
      // Method To print Linked List
         public void printList(){
