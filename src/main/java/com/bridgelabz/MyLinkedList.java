860e59dc2d0aa9f0a6d5830a72bb2bc9a3b07a30
shwetap29 <shwetaprasad48@gmail.com>
2021-06-10 22:48:25 +0530

    ADDED:Method To Search A node

--- src/main/java/com/bridgelabz/MyLinkedList.java
+++ src/main/java/com/bridgelabz/MyLinkedList.java
@@ -1,13 +1,15 @@
 package com.bridgelabz;
 
 public class MyLinkedList {
+
     public static void main(String[] args){
         LinkedList linkedList1= new LinkedList();
         linkedList1.append(56);
         linkedList1.append(70);
         linkedList1.insertAfter(linkedList1.head, 30);
         //linkedList1.pop();
-        linkedList1.popLast();
+        //linkedList1.popLast();
+        linkedList1.searchElement(30);
         linkedList1.printList();
     }
 }
@@ -25,6 +27,7 @@ public class MyLinkedList {
                 next = null;
             }
         }
+
         /*
         //Method To Add elements To linked List
         public void add (int data){
@@ -41,6 +44,7 @@ public class MyLinkedList {
              head = newNode;
              return;
          }
+
          Node last = head;
          while (last.next !=null) {
              last = last.next;
@@ -68,7 +72,7 @@ public class MyLinkedList {
          }
             head = head.next;
      }
-
+        // Method To delete Last Element
      public void popLast() {
             if (head == null)
                 System.out.println("List is Empty");
@@ -81,6 +85,28 @@ public class MyLinkedList {
                 temp.next = null;
                 last.next = temp.next;
      }
+
+        //Method To Search Element
+        public void searchElement(int data)
+        {
+            Node n=head;
+            int count=1;
+            int flag=0;
+            while (n != null)
+            {
+                if (n.data == data){
+                        flag=1;
+                        break;
+                }
+                count = count+1;
+                n=n.next;
+            }
+            if (flag==1)
+                System.out.println("Node with value "+data+" is found at position " +count);
+            else
+                System.out.println("Element is not present in the list");
+        }
+
 
      // Method To print Linked List
         public void printList(){
